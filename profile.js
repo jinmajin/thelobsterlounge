1d21bff3c5042eb5ddc5ff4bf4dff8fc3ede623d
Christopher J. Rogers <crogers3@mit.edu>
2015-04-11 21:30:52 -0400

    Added profile page. Still WOP. Changed our jquery to use version 1.11.2.

---
+++ profile.js
@@ -0,0 +1,89 @@
+$(document).ready(function() {
+  var profileInfo = profileJeff;
+  fillInProfileInfo(profileInfo);
+  
+  // Give all media a 4:3 ratio
+  setMediaHeight();
+  $(window).resize(function() {setMediaHeight()});
+
+  $('#follow-btn').click(function() {
+    var text = $('#follow-btn').text();
+    $('#follow-btn').text(text === 'Follow' ? 'Following' : 'Follow');
+  });
+});
+
+var setMediaHeight = function(ratio) {
+  // Default ratio is 4:3
+  if (!ratio) ratio = 0.75;
+  $('.media').each(function(i, media) {
+    $(media).height($(media).width() * ratio);
+  });
+}
+
+var fillInProfileInfo = function(profileInfo) {
+  $('#profile-name').text(profileInfo.profileName);
+  $('#location').text(profileInfo.location);
+  $('#genre').text(profileInfo.genre).prepend($('<span class="dot">').text('\267'));
+  $('#quote').text(profileInfo.quote).prepend($('<span class="dot">').text('\267'));
+  $('#about-me-details').text(profileInfo.aboutMe);
+
+  $('.about-me .thumbnail').append($('<img class="img-responsive" src="' + profileInfo.profilePic.src + '">'));
+
+  var performances = [];
+  profileInfo.upcomingPerformances.forEach(function(performance) {
+    performances.push($('<li>').text(performance.date + ' -- ' + performance.location));
+    performances.push(performance.details);
+  });
+  $('#upcoming-performances-list').append(performances);
+
+  mediaCollection = [];
+  profileInfo.media.forEach(function(media) {
+    if (media.type === 'video') {
+      mediaCollection.push($('<div class="thumbnail media"><iframe src="' + media.src + '" frameborder="0" allowfullscreen>'));
+    } else {
+      mediaCollection.push($('<a class="thumbnail media"><img class="img-responsive" src="' + media.src + '">'));
+    }
+  });
+  mediaCollection.forEach(function(media, i) {
+    if (i == 0) {
+      // Put into the big frame
+      $('#media-container').append($('<div class="row">').html($('<div class="col-md-8">').html(media)));
+    } else if (i == 1) {
+      // Put into side frame
+      $('#media-container .row').append($('<div class="col-md-4">').html(media));
+    } else if (i == 2) {
+      // Put into side fram
+      $('#media-container .col-md-4').append(media);
+    } else if (i % 3 == 0) {
+      // Add a new row and append
+      $('#media-container').append($('<div class="row">').html($('<div class="col-md-4">').html(media)));
+    } else {
+      // Append to last row
+      $('#media-container .row').last().append($('<div class="col-md-4">').html(media));
+    }
+  });
+};
+
+// Jeff Profile Info
+var profileJeff = {
+  profileName: "Jazzy Jeff's Jumpin' Jam",
+  location: "Boston, MA",
+  genre: "Music",
+  quote: "To play is to live",
+  aboutMe: "I've been playing jazz in the Boston area for about 7 years now. " +
+    "Before that, I used to play in Central Park NYC for 12 years. I love playing the guitar and the piano.",
+  upcomingPerformances: [
+    {date: "March 5, 2015", location: "Harvard Square", details: "I'll be playing my piano in the middle of the square from 5:00pm to 9:00pm"},
+    {date: "March 10, 2015", location: "Kendall Station", details: "I'll play my guitar on the inbound platform all day long"},
+    {date: "April 1, 2015", location: "Boston Commons", details: "I'll be playing guitar by the green line station entrance"}
+  ],
+  media: [
+    {type: 'video', src: "https://www.youtube.com/embed/EXOyjENFQMc"},
+    {type: 'image', src: "http://www.conquerblog.com/wp-content/uploads/2013/01/street-performer.jpg"},
+    {type: 'image', src: "http://diymusician.cdbaby.com/wp-content/uploads/2012/05/shutterstock_82623235.jpg"},
+    {type: 'image', src: "https://lifeintheblueridges.files.wordpress.com/2012/07/img_3765.jpg"},
+    {type: 'image', src: "http://quirkytravelguy.com/wp-content/uploads/2012/06/street-performer.jpg"},
+    {type: 'image', src: "http://nyulocal.com/wp-content/uploads/2012/04/1054.jpg"},
+  ],
+  profilePic: {src: "http://doge2048.com/meta/doge-600.png"},
+};
