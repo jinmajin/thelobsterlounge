03f31a7bb0ec6941a4c1bb376766b2c0426877fa
Christopher J. Rogers <crogers3@mit.edu>
2015-04-14 12:21:53 -0400

    Added a separate profile-resources file, where all resources associated with a profile should be stored

--- profile.js
+++ profile.js
@@ -1,11 +1,6 @@
 $(document).ready(function() {
   var profile = getParameterByName('profile');
-  var profileInfo;
-  if (profile == 'usTheDuo') {
-    profileInfo = profileUsTheDuo;
-  } else {
-    profileInfo = profileJeff;
-  }
+  var profileInfo = profileResources[profile] || profileResources.jazzyJeff;
   fillInProfileInfo(profileInfo);
   
   // Give all media a 4:3 ratio
@@ -110,47 +105,3 @@ function getParameterByName(name) {
         results = regex.exec(location.search);
     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
-
-// Jeff Profile Info
-var profileJeff = {
-  profileName: "Jazzy Jeff's Jumpin' Jam",
-  location: "Boston, MA",
-  genre: "Music",
-  quote: '"To play is to live"',
-  aboutMe: "I've been playing jazz in the Boston area for about 7 years now. " +
-    "Before that, I used to play in Central Park NYC for 12 years. I love playing the guitar and the piano.",
-  upcomingPerformances: [
-    {date: "March 5, 2015", location: "Harvard Square", details: "I'll be playing my piano in the middle of the square from 5:00pm to 9:00pm"},
-    {date: "March 10, 2015", location: "Kendall Station", details: "I'll play my guitar on the inbound platform all day long"},
-    {date: "April 1, 2015", location: "Boston Commons", details: "I'll be playing guitar by the green line station entrance"}
-  ],
-  media: [
-    {type: 'video', src: "https://www.youtube.com/embed/EXOyjENFQMc"},
-    {type: 'image', src: "http://www.conquerblog.com/wp-content/uploads/2013/01/street-performer.jpg"},
-    {type: 'image', src: "http://diymusician.cdbaby.com/wp-content/uploads/2012/05/shutterstock_82623235.jpg"},
-    {type: 'image', src: "https://lifeintheblueridges.files.wordpress.com/2012/07/img_3765.jpg"},
-    {type: 'image', src: "http://quirkytravelguy.com/wp-content/uploads/2012/06/street-performer.jpg"},
-    {type: 'image', src: "http://nyulocal.com/wp-content/uploads/2012/04/1054.jpg"},
-  ],
-  profilePic: {src: "http://doge2048.com/meta/doge-600.png"},
-};
-
-// Us The Duo Profile Info
-var profileUsTheDuo = {
-  profileName: "Us The Duo",
-  location: "New York, NY",
-  genre: "Music",
-  quote: '"We like to play music"',
-  aboutMe: "Three years ago, two emerging solo artists from opposite sides of the country randomly collided, fell in love, and combined their talents to form Us The Duo. A few successful YouTube videos (28+ million views) encouraged these pop songwriters to write their own music together, leading them to produce their first self-titled album. After its release, Michael & Carissa got married and have been traveling and playing music together ever since.",
-  upcomingPerformances: [
-    {date: "April 13, 2015", location: "Central Park", details: "We'll be playing some wicked sweet jams."},
-    {date: "April 14, 2015", location: "Central Park", details: "We'll be playing some moderately tasty jams."},
-  ],
-  media: [
-    {type: 'video', src: "https://www.youtube.com/embed/F20Ssabgf70"},
-    {type: 'video', src: "https://www.youtube.com/embed/zbyLADjgpik"},
-    {type: 'video', src: "https://www.youtube.com/embed/2nPpR_VbquA"},
-    {type: 'image', src: "http://www.ustheduo.com/files/2014/12/UsTheDuoSingapore.jpg"},
-  ],
-  profilePic: {src: "http://www.ustheduo.com/files/2014/05/UsTheDuoFeaturedImage-283x270.jpg"},
-};
