1c724012413f77902716be720087917ebaf061c1
Christopher J. Rogers <crogers3@mit.edu>
2015-04-14 12:56:23 -0400

    Made two new profiles

--- profile-resources.js
+++ profile-resources.js
@@ -13,7 +13,7 @@ var profileResources = {
       {date: "April 1, 2015", location: "Boston Commons", details: "I'll be playing guitar by the green line station entrance"}
     ],
     media: [
-      {type: 'video', src: "https://www.youtube.com/embed/EXOyjENFQMc"},
+      {type: 'video', src: "https://www.youtube.com/embed/eeHEGK0vaqA"},
       {type: 'image', src: "http://www.conquerblog.com/wp-content/uploads/2013/01/street-performer.jpg"},
       {type: 'image', src: "http://diymusician.cdbaby.com/wp-content/uploads/2012/05/shutterstock_82623235.jpg"},
       {type: 'image', src: "https://lifeintheblueridges.files.wordpress.com/2012/07/img_3765.jpg"},
@@ -35,11 +35,49 @@ var profileResources = {
       {date: "April 14, 2015", location: "Central Park", details: "We'll be playing some moderately tasty jams."},
     ],
     media: [
-      {type: 'video', src: "https://www.youtube.com/embed/F20Ssabgf70"},
+      {type: 'video', src: "https://www.youtube.com/embed/O1mmOm-1X4Q"},
       {type: 'video', src: "https://www.youtube.com/embed/zbyLADjgpik"},
       {type: 'video', src: "https://www.youtube.com/embed/2nPpR_VbquA"},
       {type: 'image', src: "http://www.ustheduo.com/files/2014/12/UsTheDuoSingapore.jpg"},
     ],
     profilePic: {src: "http://www.ustheduo.com/files/2014/05/UsTheDuoFeaturedImage-283x270.jpg"},
+  },
+
+  // Juggling George's Profile
+  jugglingGeorge: {
+    profileName: "Juggling George",
+    location: "Cambridge, MA",
+    genre: "Juggling",
+    quote: '"Jugglin\' all day every day"',
+    aboutMe: "I've been juggling for 47 years now. I started in my youth, and it has brought me to where I am today. I don't see juggling as a hobby so much as a way of life. I encourage you to join me. Join the revolution!",
+    upcomingPerformances: [
+      {date: "April 13, 2015", location: "Kendall Square", details: "I'll be juggling and playing my piano in the outbound side of the T-station."},
+      {date: "April 14, 2015", location: "MIT's Kresge Field", details: "I'll be joining the MIT Juggling Club to do some amazing juggling."},
+    ],
+    media: [
+      {type: 'video', src: "https://www.youtube.com/embed/AxHt4-GDvck"},
+      {type: 'video', src: "https://www.youtube.com/embed/OwPcwPstcH4"},
+      {type: 'image', src: "http://pianojuggler.com/press_photos/Piano_Juggler_19Print.jpg"},
+    ],
+    profilePic: {src: "http://pianojuggler.com/images/big/39-03-82Print.jpg"},
+  },
+
+  // Acrobatic Adam's Profile
+  acrobaticAdam: {
+    profileName: "Acrobatic Adam",
+    location: "New York, NY",
+    genre: "Acrobatics",
+    quote: '"I like to jump over things"',
+    aboutMe: "I've been into acrobatics for 10 years. I enjoy jumping, flipping, and free-running. Come watch me to cool things with gravity and rotational inertia.",
+    upcomingPerformances: [
+      {date: "April 13, 2015", location: "Battery Park", details: "Me and the NYC Acrobatics Street Performers group will be doing some sweet stunts."},
+      {date: "April 14, 2015", location: "Central Park", details: "I'll be jumping over more things."},
+    ],
+    media: [
+      {type: 'video', src: "https://www.youtube.com/embed/LaYEMUAPUf0"},
+      {type: 'video', src: "https://www.youtube.com/embed/bw1G_BoBbJs"},
+      {type: 'image', src: "http://www.pxleyes.com/images/contests/street-performers-2/fullsize/Street-Performer-4ee18c0dc0ae9_hires.jpg"},
+    ],
+    profilePic: {src: "https://kategale.files.wordpress.com/2010/05/venice-beach-street-performers-picture.jpg"},
   },
 };
