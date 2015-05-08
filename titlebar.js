18ea731bfc7ea06b43dc41966d9e4aa38a7f211b
Christopher J. Rogers <crogers3@mit.edu>
2015-05-08 14:29:46 -0400

    added filtering bars to followed user list and inbox

--- titlebar.js
+++ titlebar.js
@@ -51,20 +51,33 @@ var showAlert = function(message) {
 };
 
 var setUpFollowedUsers = function() {
-  var followedUsersList = $('<div>').addClass('list-group');
+  var container = $('<div>');
+  var followedUsersList = $('<div>').attr('id', 'followed-users-list').addClass('list-group');
   profileResources['jazzyJeff'].following.forEach(function(user) {
     followedUsersList.append($('<a>').addClass('list-group-item').text(user).attr('href', 'profile.html?profile=' + user));
   });
+  var filter = $('<div>').append($('<input>').addClass('form-control').attr('id', 'followed-users-filter').attr('placeholder', 'Enter User\'s Username')
+  .on('input propertychange paste', function() {
+    var value = $('#followed-users-filter').val().toLowerCase();
+    followedUsersList.children().each(function(i, link) {
+      if ($(link).text().toLowerCase().indexOf(value) == -1) {
+        $(link).addClass('hidden');
+      } else {
+        $(link).removeClass('hidden');
+      }
+    })
+  }));
+  container.append(filter).append(followedUsersList);
   $('#show-followed-btn').popover({
-    content: followedUsersList,
+    content: container,
     html: true,
   });
-  $('#show-followed-btn').data('bs.popover').options.content = followedUsersList;
+  $('#show-followed-btn').data('bs.popover').options.content = container;
 }
 
 $(document).on('click', function (e) {
   if ($(e.target).attr('id') == 'show-followed-btn') {
-    $('#show-followed-btn').popover('toggle');
+    $('#show-followed-btn').popover('toggle').find('#followed-users-filter');
     $('#show-inbox-btn').popover('hide');
   } else if ($(e.target).attr('id') == 'show-inbox-btn') {
     $('#show-inbox-btn').popover('toggle');
@@ -75,14 +88,31 @@ $(document).on('click', function (e) {
 });
 
 var setUpInbox = function() {
-  var messagesList = $('<div>').addClass('list-group');
+  var container = $('<div>');
+  var messagesList = $('<div>').attr('id', 'inbox').addClass('list-group');
   profileResources['jazzyJeff'].messages.forEach(function(message) {
     var from = $('<div>').append($('<a>').text(message.from).attr('href', 'profile.html?profile=' + message.from));
     var body = $('<p>').text(message.body);
     messagesList.append($('<div>').addClass('list-group-item').append(from).append(body));
   });
+  var filter = $('<div>').append($('<input>').addClass('form-control').attr('id', 'inbox-filter').attr('placeholder', 'Search')
+  .on('input propertychange paste', function() {
+    var value = $('#inbox-filter').val().toLowerCase();
+    messagesList.children().each(function(i, message) {
+      var from = $(message).find('a').text().toLowerCase();
+      var body = $(message).find('p').text().toLowerCase();
+      console.log(from);
+      console.log(body);
+      if (from.indexOf(value) == -1 && body.indexOf(value) == -1) {
+        $(message).addClass('hidden');
+      } else {
+        $(message).removeClass('hidden');
+      }
+    })
+  }));
+  container.append(filter).append(messagesList);
   $('#show-inbox-btn').popover({
-    content: messagesList,
+    content: container,
     html: true,
   });
 }
